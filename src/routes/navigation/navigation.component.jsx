16a503e033b5e1949a59b5db1eee95ae202ddf1e
Jose Guerra <josemguerra@Joses-Mac-Pro.fritz.box>
2023-08-17 14:20:23 +0100

    Add routing and React-Router

---
+++ src/routes/navigation/navigation.component.jsx
@@ -0,0 +1,28 @@
+import { Fragment } from "react";
+import { Outlet, Link } from "react-router-dom";
+import "./navigation.styles.scss";
+import { ReactComponent as BrandLogo } from "../../assets/crown.svg";
+
+const Navigation = () => {
+  return (
+    <>
+      <nav>
+        <ul className="main-nav">
+          <li className="logo-container">
+            <Link className={"logo"} to={"/"}>
+              <BrandLogo />
+            </Link>
+          </li>
+          <li className="nav-link push">
+            <Link to={"shop"}>
+              <h1>Shop</h1>
+            </Link>
+          </li>
+        </ul>
+      </nav>
+      <Outlet />
+    </>
+  );
+};
+
+export default Navigation;
